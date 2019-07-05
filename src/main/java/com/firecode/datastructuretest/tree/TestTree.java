49466a29372e02757b7919a6525897118a0cbcc5
jiangcihuo <chiang-fire@outlook.com>
2019-07-05 10:44:45 +0800

    fix

--- src/main/java/com/firecode/datastructuretest/tree/TestTree.java
+++ src/main/java/com/firecode/datastructuretest/tree/TestTree.java
@@ -1,8 +1,8 @@
 package com.firecode.datastructuretest.tree;
 
 /**
- * Tree(树)的深度遍历算法，快速查找子节点。
- * @see 
+ * Tree(树)型深度遍历算法，快速查找子节点。
+ * @see https://github.com/firechiang/data-structure-test/blob/master/image/depth-first-tree.svg
  * @author JIANG
  */
 public class TestTree {
